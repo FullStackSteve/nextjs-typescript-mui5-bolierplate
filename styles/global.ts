937730b425cd60a6780f40c7a485bc309a2cd0c2
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-13 10:33:41 +0000

    Added some global styles

---
+++ styles/global.ts
@@ -0,0 +1,18 @@
+const GlobalStyles = {
+   flexBox: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   transparentBox: {
+      backgroundColor: 'transparent',
+      boxShadow: 'none'
+   },
+   stretchBox: {
+      minWidth: '80%',
+      minHeight: '70%'
+   }
+}
+
+export default GlobalStyles
