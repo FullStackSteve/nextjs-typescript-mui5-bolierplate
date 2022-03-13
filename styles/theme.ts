ab99c6758716a1d66bb78c3bb7e0c1bee3f1a23c
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-13 18:19:22 +0000

    Add overrides to muitheme

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,11 +1,16 @@
 // When using TypeScript 4.x and above
 import type {} from '@mui/lab/themeAugmentation'
 
-import { createTheme, responsiveFontSizes } from '@mui/material/styles'
-import { palette } from 'constants/theme/palette'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles' // import { TypographyOptions } from '@mui/material/styles/createTypography'
+import { palette, components, typography } from 'constants/theme'
 
 let theme = createTheme({
-   palette: palette
+   // MUI Palette overrides
+   palette: palette,
+   // Component style overrides
+   components: components,
+   // Typography styles overrides
+   typography: typography
 })
 
 theme = responsiveFontSizes(theme)
