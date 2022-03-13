609ad9d2bcd658b8dd5a764ebc48409e1da41ddd
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-13 18:11:08 +0000

    Added custom gradient to mui palette

--- constants/theme/palette/index.ts
+++ constants/theme/palette/index.ts
@@ -3,6 +3,17 @@ import { red, yellow, purple, green } from '@mui/material/colors'
 import { PaletteOptions } from '@mui/material/styles/createPalette'
 import { black, white } from '../colors'
 
+declare module '@mui/material/styles' {
+   interface Palette {
+      gradient: Palette['background']
+   }
+
+   // allow configuration using `createTheme`
+   interface PaletteOptions {
+      gradient?: PaletteOptions['background']
+   }
+}
+
 const paletteMode: PaletteMode = 'light'
 
 export const palette: PaletteOptions = {
@@ -24,5 +35,12 @@ export const palette: PaletteOptions = {
    success: {
       main: green['600']
    },
-   mode: paletteMode
+   mode: paletteMode,
+   background: {
+      default: white['900'],
+      paper: white['A700']
+   },
+   gradient: {
+      default: `linear-gradient(45deg, ${purple['500']} 15%, ${purple['A700']} 90%)`
+   }
 }
