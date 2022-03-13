2ba7065aec1710dae55bc099ec57a7228a0a7890
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-13 17:07:25 +0000

    MuiButtonstyle override

---
+++ constants/theme/components/MuiButton/index.ts
@@ -0,0 +1,12 @@
+export const MuiButton = {
+   MuiButton: {
+      styleOverrides: {
+         root: {
+            borderRadius: 5,
+            margin: 5,
+            paddingLeft: 20,
+            paddingRight: 20
+         }
+      }
+   }
+}
