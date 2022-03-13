a8acfb08ecb4e872000b342a68b66a29fedff142
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-13 19:45:54 +0000

    Buttons responsively aligned

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,45 @@ import type { NextPage } from 'next'
 import { Layout } from 'features/ui/containers'
 import { Hero, Link } from 'features/ui/components'
 import { HeroConstants } from 'constants/pages/home/hero'
-import { Box, Button } from '@mui/material'
+import { Grid, Box, Button, useTheme, useMediaQuery } from '@mui/material'
 import { logo } from 'constants/images'
+import GlobalStyles from 'styles/global'
+import {} from '@mui/material/styles'
 
 const Home: NextPage = () => {
    const { heading, subHeading, buttons } = HeroConstants
+   const theme = useTheme()
+   const isBig = useMediaQuery(theme.breakpoints.up('sm'))
 
    return (
       <Layout>
          <Hero imageSrc={logo.svg} heading={heading.text} subHeading={subHeading.text}>
-            {buttons.map((button, index) => {
-               return (
-                  <Box maxWidth="sm" key={index}>
-                     <Button
-                        variant="contained"
-                        color="primary"
-                        component={Link}
-                        noLinkStyle
-                        href={button.href}
-                     >
-                        {button.text as string}
-                     </Button>
-                  </Box>
-               )
-            })}
+            <Grid
+               container
+               spacing={1}
+               sx={{
+                  ...GlobalStyles.flexGrid,
+                  maxWidth: isBig ? 400 : undefined
+               }}
+            >
+               {buttons.map((button, index) => {
+                  return (
+                     <Grid item xs={12} sm={4} md={4} key={index}>
+                        <Box sx={{ ...GlobalStyles.flexBox }}>
+                           <Button
+                              variant="contained"
+                              color="primary"
+                              component={Link}
+                              noLinkStyle
+                              href={button.href}
+                           >
+                              {button.text as string}
+                           </Button>
+                        </Box>
+                     </Grid>
+                  )
+               })}
+            </Grid>
          </Hero>
       </Layout>
    )
