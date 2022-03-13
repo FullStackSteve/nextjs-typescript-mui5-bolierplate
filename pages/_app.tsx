6bcc7f7d9221c1bb5ed107ac5eb93e460d6727b1
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-13 11:54:42 +0000

    Refactored _app to use CustomApp component with MUI theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import theme from 'styles/theme'
+import { createEmotionCache } from 'features/mui/functions'
 
-function MyApp({ Component, pageProps }: AppProps) {
-   return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface CustomAppProps extends AppProps {
+   emotionCache?: EmotionCache
 }
 
-export default MyApp
+export default function CustomApp(props: CustomAppProps) {
+   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+   return (
+      <CacheProvider value={emotionCache}>
+         <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+         </Head>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+         </ThemeProvider>
+      </CacheProvider>
+   )
+}
